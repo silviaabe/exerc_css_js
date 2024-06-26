970f63a1ebce48345f72bcb6fdd5885501c80b53
Silvia Abe <silviaabebuzatto@gmail.com>
2024-06-26 16:17:52 -0300

    atualizando

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
 import { Container } from './styles'
 
 function App() {
